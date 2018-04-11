170996a66b1cb4906f08de76481cf729bedb7ca5
Adarsh Anand <adarsh.anand15@gmail.com>
2018-04-11 21:45:33 +0530

    ok

---
+++ main_gui.m
@@ -0,0 +1,73 @@
+function varargout = main_gui(varargin)
+% MAIN_GUI MATLAB code for main_gui.fig
+%      MAIN_GUI, by itself, creates a new MAIN_GUI or raises the existing
+%      singleton*.
+%
+%      H = MAIN_GUI returns the handle to a new MAIN_GUI or the handle to
+%      the existing singleton*.
+%
+%      MAIN_GUI('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in MAIN_GUI.M with the given input arguments.
+%
+%      MAIN_GUI('Property','Value',...) creates a new MAIN_GUI or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before main_gui_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to main_gui_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help main_gui
+
+% Last Modified by GUIDE v2.5 11-Apr-2018 21:42:51
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @main_gui_OpeningFcn, ...
+                   'gui_OutputFcn',  @main_gui_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before main_gui is made visible.
+function main_gui_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to main_gui (see VARARGIN)
+
+% Choose default command line output for main_gui
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes main_gui wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = main_gui_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
